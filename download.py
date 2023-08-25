20e1de41f8df0f9b3610989595332ec28e83833f
wimvanhenden-tool <125361595+wimvanhenden-tool@users.noreply.github.com>
2023-08-25 18:44:51 +0200

    Update download.py

--- download.py
+++ download.py
@@ -2,10 +2,18 @@
 
 # In this example: A Huggingface BERT model
 from transformers import pipeline
+import torch
+import base64
+import numpy as np
+from PIL import Image
+from io import BytesIO
+from diffusers.utils import load_image
+from diffusers import UniPCMultistepScheduler, StableDiffusionControlNetPipeline, ControlNetModel
 
 def download_model():
-    # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    controlnet = ControlNetModel.from_pretrained("lllyasviel/control_v11f1p_sd15_depth",torch_dtype=torch.float16)
+    model = StableDiffusionControlNetPipeline.from_pretrained("runwayml/stable-diffusion-v1-5",controlnet=controlnet,torch_dtype=torch.float16)
+    depth_estimator = pipeline('depth-estimation')
 
 if __name__ == "__main__":
     download_model()
